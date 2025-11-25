7882f38b9ee7be69722356ec8ac4b8157f6f0ef7
jsapinat <jose.bras@cern.ch>
2025-11-25 19:07:27 +0100

    Small fix

--- src/avtools/landb/device.py
+++ src/avtools/landb/device.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from typing import Any
 from collections.abc import Mapping
+from typing import Any
 
 import structlog
 from pydantic import BaseModel, ConfigDict, Field
